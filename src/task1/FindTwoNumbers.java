7ff3542f08ab3a158fc4911a9df06b828ff326cf
darknight112 <hkalamri@gmail.com>
2023-01-14 23:58:40 -0800

    add documantation to find the two numbers that appear only once in the array

--- src/task1/FindTwoNumbers.java
+++ src/task1/FindTwoNumbers.java
@@ -7,25 +7,25 @@ public class FindTwoNumbers {
 	public static void main(String[] args) {
 		// Given an array of integers, find the two numbers that appear only once in the array.
 		int[] num= {900,1200,1200,1210,900,750};
-        Stack<Integer> stack = new Stack<Integer>();
+        Stack<Integer> stack = new Stack<Integer>(); //stack that will save the only two numbers that appear once.
         
-	    for (int i = 0; i < 6; i++) {
+	    for (int i = 0; i < 6; i++) { 
 	    	 
-	        int count = 0;
+	        int count = 0; //initialize count that will count the number if it appear more than one.
 	   
-	        for (int j = 0; j < 6; j++) {
+	        for (int j = 0; j < 6; j++) { //second loop to check each number with the array.
 	   
-	          if (num[i] == num[j]) {
+	          if (num[i] == num[j]) { //if num[i] (the checked number) will equal to other number in array -> count++.
 	            count++;
 	          }
 	        }
 
-	        if (count == 1) {
+	        if (count == 1) { //if the number appear only once in the array will be pushed to the stack.
 	          stack.push(num[i]);
 	        }
 	      }
 	    
-	    System.out.println(stack);
+	    System.out.println(stack); //print stack (two numbers that appear once).
 
 
 
