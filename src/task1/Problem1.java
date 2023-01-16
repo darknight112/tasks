6a987bf91110641cd5959a07115292d62f9ff299
darknight112 <hkalamri@gmail.com>
2023-01-15 22:52:41 -0800

    update problem 1

--- src/task1/Problem1.java
+++ src/task1/Problem1.java
@@ -9,21 +9,30 @@ public class Problem1 {
 		
 		
 		int[] num= new int[] {1, 2, 3, 4, 5, 6}; // step 1 create array of integers
-		Arrays.sort(num);  // step 2 sort the array 
 		//int[] highestFour= new int[4]; //step 3 create new array that will take highest four numbers from (num array)
-		int product=1; //product will be the result of product the highest four numbers in array.
+		
 
 		//int count=0; // count will be responsible for the index number in highestFour array.
 		
-		for(int i=2; i<6;i++) { //this loop to calculate the product result
-			product=num[i]*product;
-		}
+		int a= maxProduct(num); //caling maxProduct function
+		
+
 		
 		
 		
-		System.out.println(product + " = ( " + num[num.length-1] + " * "+num[num.length-2] + " * " + num[num.length-3] + " * " + num[num.length-4] + ")");
 
 
 	}
+	public static int maxProduct(int num[]) { // method to find largest 4 numbers in the array from user then return the sum of the product.
+		int product=1; //product will be the result of product the highest four numbers in array.
+		Arrays.sort(num);  // step 2 sort the array 
+		for(int i=2; i<6;i++) { //this loop to calculate the product result
+			product=num[i]*product;
+		}
+		System.out.println(product + " = ( " + num[num.length-1] + " * "+num[num.length-2] + " * " + num[num.length-3] + " * " + num[num.length-4] + ")");
 
+		return product;
+	}
 }
+
+
---
+++ src/task1/Problem4.java
@@ -0,0 +1,13 @@
+package task1;
+
+public class Problem4 {
+
+	public static void main(String[] args) {
+		// Problem: Given an array of integers and a number k, write a function to determine if there are two distinct elements in the array that add up to k and both elements should be even number.
+		// Input: [1, 2, 4, 6, 8, 10], k = 10
+		// Output: true (2 + 8 = 10)
+		
+		
+	}
+
+}
