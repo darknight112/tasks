13dcff781e75167f51add82e4b24ef27a08e4573
darknight112 <hkalamri@gmail.com>
2023-01-18 03:54:07 -0800

    Problem 10 you would need to provide an array of integers as input to the function that finds the next greater element.

--- src/task1/Problem10.java
+++ src/task1/Problem10.java
@@ -4,8 +4,10 @@ public class Problem10 {
 
 	public static void main(String[] args) {
 		// Problem 10 you would need to provide an array of integers as input to the function that finds the next greater element.
-
-		int[] numbers = { 1, 2, 20, 15, 15, 30, 15, 15 };
+		
+		int[] numbers = { 1,2,3,4,5 };
+		Arrays.sort(numbers);
+		System.out.println("The second largest element is "+ numbers[numbers.length-2]);
 		
 
 	}
